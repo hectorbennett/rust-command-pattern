9b0a9fdbb843bd2287fc5e9f902e980e76ae58c7
Hector <hectorbennett@outlook.com>
2023-03-30 14:42:06 +0100

    Add new test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 pub mod test_1;
 pub mod test_3;
+pub mod test_4;
 
 // use test_1::test_1;
-use test_3::test_3;
+// use test_3::test_3;
+use test_4::test_4;
 
 fn main() {
     // test_1();
-    test_3();
+    // test_3();
+    test_4();
 }
